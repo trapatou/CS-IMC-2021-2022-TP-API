7eecdcea80e158a60bc0ae5474b3d55933adb5bf
Luc Vo Van <luc.vovan@outlook.com>
2022-01-13 02:19:50 +0000

    fixing

--- Query1/__init__.py
+++ Query1/__init__.py
@@ -1,7 +1,7 @@
 import logging
-from py2neo import Graph
-from py2neo.bulk import create_nodes, create_relationships
-from py2neo.data import Node
+# from py2neo import Graph
+# from py2neo.bulk import create_nodes, create_relationships
+# from py2neo.data import Node
 import os
 import pyodbc
 import azure.functions as func
@@ -32,20 +32,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
         return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
         
-    try:
-        logging.info("Test de connexion avec py2neo...")
-        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
-        graph.run("MATCH (n:Test) RETURN n")
+    # try:
+    #     logging.info("Test de connexion avec py2neo...")
+    #     graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+    #     graph.run("MATCH (n:Test) RETURN n")
 
-        # try:
-        #     logging.info("Test de connexion avec pyodbc...")
-        #     with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-        #         cursor = conn.cursor()
-        #         cursor.execute("SELECT 1")
-        # except:
-        #     errorMessage = "Erreur de connexion a la base SQL"
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT 1")
     except:
-        errorMessage = "Erreur de connexion a la base Neo4j"
+        errorMessage = "Erreur de connexion a la base SQL"
+    # except:
+    #     errorMessage = "Erreur de connexion a la base Neo4j"
         
     finalMessage = "Le parametre name n'a pas ete fourni lors de l'appel."
     if name:
