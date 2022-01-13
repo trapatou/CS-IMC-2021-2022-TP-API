0bb740b747052a3d7b5ad82dd8fa4fd5534937f3
Luc Vo Van <luc.vovan@outlook.com>
2022-01-13 00:54:47 +0000

    Added tests in Query1

--- Query1/__init__.py
+++ Query1/__init__.py
@@ -1,5 +1,9 @@
 import logging
-
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc
 import azure.functions as func
 
 
@@ -14,11 +18,43 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    try:
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        graph.run("MATCH (n:Test) RETURN n")
+
+        try:
+            logging.info("Test de connexion avec pyodbc...")
+            with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT 1")
+        except:
+            errorMessage = "Erreur de connexion a la base SQL"
+    except Exception as error:
+        errorMessage = "Erreur de connexion a la base Neo4j"
+        
+    finalMessage = "Le parametre name n'a pas ete fourni lors de l'appel."
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        finalMessage = f"Hello, {name}!"
+    
+    if errorMessage is None:
+        finalMessage = finalMessage + errorMessage
+        func.HttpResponse(finalMessage, status_code=500)
+
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        finalMessage = finalMessage + "Connexions réussies a Neo4j et SQL!"
+        func.HttpResponse(finalMessage)
